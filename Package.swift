650a7db071bb5f6ca1ba0343616a9c131afe6634
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2023-09-10 14:43:06 +0900

    Drop Carthage

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "VTuberKit",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(name: "VTuberKit", targets: ["VTuberKit"])
     ],
